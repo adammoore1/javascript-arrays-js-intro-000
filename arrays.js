8f0777ed75db71f7bffb3735f9e7c86371a6d08d
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-23 18:17:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
-["mars", ...chocolateBars];
+chocolateBars[1]="mars"
 }
