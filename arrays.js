c600da291bda5aec56945cea464b272baf46a222
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-23 17:34:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-function addElementToBeginningOfArray(chocolateBars, element){
+function addElementToBeginningOfArray(array, element){
 chocolateBars.unshift("mars");
 }
